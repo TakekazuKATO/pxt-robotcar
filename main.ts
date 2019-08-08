268b3c28608d0691c0fdc68f8afc73f92896a02d
TakekazuKATO <kato.takekazu@sist.ac.jp>
2019-08-09 08:43:14 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum Direction {
+    //% block="左",
+    Left,
+    //% block="右",
+    Right
+}
+//% block
+//% speed.shadow="speedPicker"
+namespace robotCar {
+    export function wheel(dir: Direction, speed:number){
+
+    }
+}
