d50cbac1cbacaf7e90eabafa9bd203c8c10b9df1
TakekazuKATO <kato.takekazu@sist.ac.jp>
2019-08-11 15:39:17 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -4,10 +4,27 @@ enum Direction {
     //% block="右",
     Right
 }
-//% block
+//% block="%dir のタイヤを早さ $speed %で動かす"
 //% speed.shadow="speedPicker"
+//% speed.defl=0
 namespace robotCar {
-    export function wheel(dir: Direction, speed:number){
-
+    export function wheel(dir: Direction, speed: number) {
+        if (dir == Direction.Left) {
+            if (speed < 0) {
+                pins.analogWritePin(AnalogPin.P14, 0)
+                pins.analogWritePin(AnalogPin.P13, Math.abs(speed))
+            } else {
+                pins.analogWritePin(AnalogPin.P13, 0)
+                pins.analogWritePin(AnalogPin.P14, speed)
+            }
+        } else {
+            if (speed < 0) {
+                pins.analogWritePin(AnalogPin.P16, 0)
+                pins.analogWritePin(AnalogPin.P15, Math.abs(speed))
+            } else {
+                pins.analogWritePin(AnalogPin.P15, 0)
+                pins.analogWritePin(AnalogPin.P16, speed)
+            }
+        }
     }
 }
